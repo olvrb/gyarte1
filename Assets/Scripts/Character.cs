31a617ca50952fa104a5c416432573546a3f7546
Oliver Boudet <oboudet1@me.com>
2019-10-25 10:33:53 +0200

    char stuff

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Character : MonoBehaviour
+    {
+        public uint Hp { get; protected set; }
+
+        public void Damage(uint amount) {
+            Hp -= amount;
+        }
+    }
+}
